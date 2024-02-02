65dc181be8118a725e7328b87e3f8663a1a58696
Ronit9625 <140943640+Ronit9625@users.noreply.github.com>
2024-02-02 13:18:42 +0530

    Create process_form.php

---
+++ process_form.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "prefinpay@gmail.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
+
+    mail($to, $subject, $message, $headers);
+
+    echo "Form submitted successfully!";
+} else {
+    echo "Error submitting form.";
+}
+?>
